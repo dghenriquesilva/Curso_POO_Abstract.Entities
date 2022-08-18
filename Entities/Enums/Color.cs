f6de095d4e70f0dbc8952b8cb484b5f6ac52b074
Diogo Henrique <dghenriquesilva@gmail.com>
2022-08-17 22:13:49 -0300

    classes shape, circle e rectangule criadas

---
+++ Entities/Enums/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_POO_Abstract.Entities.Enums
+{
+    public enum Color: int
+    {
+    }
+}
